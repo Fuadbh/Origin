24a1362db2754d448cdca1609310572f477eeb7c
Fuadbh <fuabh504@gmail.com>
2022-11-14 18:07:53 +0600

    V1

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
     public static void main(String[] args) 
     {
-        System.out.println("Sadia, Big fan!");
+        System.out.println("Sadia, Medium Fan!");
     }
 }
